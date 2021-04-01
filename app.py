b29dc11c438fe364184a88683719a3c122ecfda1
FabricioGalvani <fabriciogalvani1@hotmail.com>
2021-04-01 13:18:50 -0300

    add glue app

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from aws_cdk import core
 
 from data_platform.data_lake.stack import DataLakeStack
+from glue_catalog.stack import GlueCatalogStack
 
 
 app = core.App()
 data_lake = DataLakeStack(app)
+glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
