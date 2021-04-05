ca0c0a97917da3581e324107d5147999d19a7877
FabricioGalvani <fabriciogalvani1@hotmail.com>
2021-04-05 08:24:42 -0300

    add stack athena

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from athena.stack import AthenaStack
 from aws_cdk import core
 
 from data_platform.data_lake.stack import DataLakeStack
@@ -7,4 +8,5 @@ from glue_catalog.stack import GlueCatalogStack
 app = core.App()
 data_lake = DataLakeStack(app)
 glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
+athena_stack = AthenaStack(app)
 app.synth()
