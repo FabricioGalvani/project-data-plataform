556b1611c0d9ba4c0aa2ec6e10dfe264887534d7
FabricioGalvani <fabriciogalvani1@hotmail.com>
2021-04-06 10:18:37 -0300

    test

--- app.py
+++ app.py
@@ -3,19 +3,14 @@ from aws_cdk import core
 
 from data_platform.data_lake.stack import DataLakeStack
 from glue_catalog.stack import GlueCatalogStack
+from kinesis.stack import KinesisStack
 
 
 app = core.App()
 
 data_lake = DataLakeStack(app)
-glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake_raw_bucket)
+glue_catalog = GlueCatalogStack(app, data_lake_bucket=data_lake.data_lake_raw_bucket)
 athena_stack = AthenaStack(app)
 kinesis_stack = KinesisStack(app, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 
-synth()
-
-
-
-
-
-
+app.synth()
